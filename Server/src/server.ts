10d7c23819f46bd6c6e106cb64513f3bcd319a1a
Eric Ponce <ponceeric458@gmail.com>
2025-03-26 12:17:25 -0700

    create io event listeners to establish connections

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,15 +1,43 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-const PORT = 8000;
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const rooms: any = {};
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello, TypeScript!");
+io.on("connection", (socket) => {
+  // joins the room on connection or creates room if it doesn't exist
+  socket.on("join room", (roomID: any) => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id);
+    } else {
+      rooms[roomID] = [socket.id];
+    }
+
+    // notifies users when joining the room
+    const otherUser = rooms[roomID].find((id: any) => id !== socket.id);
+    if (otherUser) {
+      socket.emit("other user", otherUser);
+      socket.to(otherUser).emit("user joined", socket.id);
+    }
+  });
+
+  socket.on("offer", (payload: any) => {
+    io.to(payload.target).emit("offer", payload);
+  });
+
+  socket.on("answer", (payload: any) => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", (incoming: any) => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(() => {
+  console.log(`Server is running`);
 });
